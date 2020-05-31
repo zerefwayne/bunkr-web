f336499169a948f5aa0ee1d8db92a7f93653514e
Aayush Joglekar <aayushjog@gmail.com>
2020-06-01 02:01:10 +0530

    removes template files

--- src/store/template/actions.type.js
+++
@@ -1,2 +0,0 @@
-export const FETCH_COURSES = "fetchCourses";
-export const FETCH_COURSE = "fetchCourse";
--- src/store/template/mutations.type.js
+++
@@ -1,2 +0,0 @@
-export const SET_ACTIVE_COURSE = "setActiveCourse";
-export const SET_COURSES = "setCourses";
