38ac13025aa8159a412673a8728e667a720aa3c5
Aayush Joglekar <aayushjog@gmail.com>
2020-06-01 16:26:32 +0530

    displays all courses in profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -2,20 +2,54 @@
   <div>
     <h3>Profile</h3>
     <button class="btn btn-sm btn-danger" @click="handleLogout">Logout</button>
+    <div class="profile-courses">
+      <h4>Add Course</h4>
+      <form>
+        <div class="form-group">
+          <label for="exampleFormControlSelect1">Course</label>
+          <select
+            class="form-control"
+            id="exampleFormControlSelect1"
+          >
+            <option
+              v-for="course in courses"
+              :key="course.code"
+              :value="course.code"
+            >{{ `${course.name} - ${course.code}` }}</option>
+          </select>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
+import { mapGetters } from "vuex";
 import { LOGOUT } from "../store/auth/actions.type";
+import { FETCH_ALL_COURSES } from "../store/course/actions.type";
 export default {
+  data() {
+    return {
+      
+    }
+  },
   computed: {
-    ...mapState({
-      user: state => state.auth.user,
-      loggedIn: state => state.auth.isAuthenticated
+    ...mapGetters({
+      user: "user",
+      isAuthenticated: "isAuthenticated",
+      courses: "courses"
     })
   },
-  mounted() {},
+  mounted() {
+    let store = this.$store;
+    Promise.all([store.dispatch(FETCH_ALL_COURSES)])
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  },
   methods: {
     handleLogout() {
       this.$store.dispatch(LOGOUT).then(() => {
